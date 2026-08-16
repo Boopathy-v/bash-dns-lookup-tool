20eb5b242dc7c85aa9b3a61961976678e45e2356
Boopathy-v <boopathybme@gmail.com>
2026-08-16 03:04:53 +0000

    new version

--- lookup.sh
+++ lookup.sh
@@ -9,10 +9,10 @@ while read -r i || [ -n "$i" ]
 do  
     if is_ip "$i"
     then
-        output=$(dig -x $i +short)   # this is REVERSE lookup, but you labeled it "forward"
+        output=$(dig -x $i +short +time=5)   # this is REVERSE lookup, but you labeled it "forward"
     
     else
-        output=$(dig $i +short)      # this is FORWARD lookup, but you labeled it "Reverse lookup"
+        output=$(dig $i +short +time=5)      # this is FORWARD lookup, but you labeled it "Reverse lookup"
     fi
 
     #To update status in log file
